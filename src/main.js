3244ed9016c002ecc81fd92514b8dd462b769fd8
sijia.hao <sijia.hao@shopee.com>
2023-07-04 20:25:44 +0800

    feat: add package for icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import count from './utils/count'
 import sum from './utils/sum'
 
 // 要想webpack打包资源，必须引入该资源，css等样式资源也不例外
+import './assets/icons/iconfont.css'
+
 import './styles/index.css'
 import './styles/index.less'
 import './styles/index.sass'
--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,16 @@ module.exports = {
                     filename: 'images/[hash:10][ext][query]'
                 }
             },
+            {
+                test: /\.(ttf|woff2?)$/,
+                type: "asset/resource", // 相当于file-loader，只会对文件原封不动输出，不转base64
+                generator: {
+                    // 指定生成的图片存放路径 + 名称
+                    // hash是随机哈希值，ext是原文件扩展名，query是url中的查询参数（如有）
+                    // :10是指哈希值只取前10位，防止名称过长
+                    filename: 'fonts/[hash:10][ext][query]'
+                }
+            },
         ],
     },
     // 插件
